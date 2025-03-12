b81a27bbfbd537137e102f05af623a072f3739ca
Gabriela870302 <gabrielamoralesmsc@gmail.com>
2025-03-12 03:19:03 -0600

    Se adiciona funcion para sortear amigo

--- app.js
+++ app.js
@@ -38,3 +38,21 @@ function actualizarListaAmigos() {
         listaAmigos.appendChild(li);
     }
 }
+
+
+//funcion sortear amigos
+function sortearAmigo() {
+    //Validacio de amigos disponibles
+    if (amigos.length === 0) {
+        alert("No es posible sortear, debe ingrear por lo menos 1 nombre");
+    } else {
+        //Se genera el indice aleatorio
+        let i = Math.floor(Math.random() * amigos.length);
+
+        //Se obtiene el nombre sorteado
+        let nombreAmigoSecreto = amigos[i];
+
+        //Se muestra el resultado
+        document.getElementById("resultado").innerHTML = "<li>El amigo secreto es: " + nombreAmigoSecreto + "</li>";
+    }
+}
